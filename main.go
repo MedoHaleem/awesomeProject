7da9117511cca4e585db7a8eceadd9d7523a057a
Medo Haleem <medo_e3@hotmail.com>
2019-04-13 12:44:20 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"awesomeProject/views"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+var (
+	homeView *views.View
+	contactView *views.View
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(contactView.Render(w, nil))
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ</h1><p>List of the most common questions</p>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Sorry, but we couldn't find the page you were looking for</h1>")
+}
+
+
+func main() {
+	homeView = views.NewView("bootstrap","views/home.gohtml")
+	contactView = views.NewView("bootstrap","views/contact.gohtml")
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
