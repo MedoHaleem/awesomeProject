947d78687dc310aa8265a9236639f0bcb9f9b07a
Medo Haleem <medo_e3@hotmail.com>
2019-04-14 15:31:38 +0200

    Created Static Controller

--- main.go
+++ main.go
@@ -2,32 +2,10 @@ package main
 
 import (
 	"awesomeProject/controllers"
-	"awesomeProject/views"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
-
-var (
-	homeView    *views.View
-	contactView *views.View
-	faqView     *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(faqView.Render(w, nil))
-}
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -36,15 +14,13 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFound)
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/faq", faq).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/faq", staticC.Faq).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", r)
--- views/view.go
+++ views/view.go
@@ -15,22 +15,29 @@ func NewView(layout string, files ...string) *View {
 
 	return &View{
 		Template: t,
-		Layout: layout,
+		Layout:   layout,
 	}
 
 }
 
 type View struct {
 	Template *template.Template
-	Layout string
+	Layout   string
+}
+
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := v.Render(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "text/html")
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
 
 func layoutFiles() []string {
-	files, err:= filepath.Glob("views/layouts/*.gohtml")
+	files, err := filepath.Glob("views/layouts/*.gohtml")
 	if err != nil {
 		panic(err)
 	}
